a9f29df8ac9fe4884627d81d594ab05640a6ff52
Arpan-gl <arpang754@gmail.com>
2026-02-12 20:39:51 +0530

    add some production files and changes some logic

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,11 +1,4 @@
-import 'dotenv/config'
 import { buildApp } from '../src/app.js'
-
-// Validate MASTER_KEY at startup
-if (!process.env.MASTER_KEY) {
-  console.error('FATAL: MASTER_KEY environment variable is not defined');
-  process.exit(1);
-}
 
 let app: any
 
